3662cfbfd47ed9a027423b29319d5201cfde79d6
FaisalTmz007 <faisaltemas007@gmail.com>
2023-06-07 21:55:10 +0700

    update return update_post

--- pages/api/post.js
+++ pages/api/post.js
@@ -85,7 +85,7 @@ postController.put('/feed/:id', verifyToken, async (req, res) => {
             console.log(thePost)
             const  { _id , createdAt,  updatedAt,  __v, likes, photo, location,  userId , ...updatedPost} = thePost._doc
             const decUpdatedPost = rsa.decryptedObjectValues(updatedPost, process.env.PRIVATE_RSA_KEY)
-            return res.status(200).json({...decUpdatedPost, _id , createdAt, updatedAt, __v, likes,userId, location})
+            return res.status(200).json({desc, _id , createdAt, updatedAt, __v, likes,userId, location})
         } else {
             return res.status(403).json({msg: "You cant edit this post"})
         }
